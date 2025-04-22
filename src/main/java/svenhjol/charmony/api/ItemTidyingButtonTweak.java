febd10130ed6c1c8fc5d5dc8608dda76fce8d9cb
Sven <sven.hjol@protonmail.com>
2025-04-22 22:48:27 +0100

    Add container and player XY offsets to tweaks. 1.15.0

--- src/main/java/svenhjol/charmony/api/ItemTidyingButtonTweak.java
+++ src/main/java/svenhjol/charmony/api/ItemTidyingButtonTweak.java
@@ -10,5 +10,18 @@ import net.minecraft.client.gui.screens.Screen;
 public interface ItemTidyingButtonTweak {
     Class<? extends Screen> getScreen();
 
+    /**
+     * @deprecated since 1.15.0
+     */
     Pair<Integer, Integer> getXYOffset();
+
+    /**
+     * Provide hardcoded container inventory X and Y base coordinates for the given screen.
+     */
+    Pair<Integer, Integer> getContainerXYOffset();
+
+    /**
+     * Provide hardcoded player inventory X and Y base coordinates for the given screen.
+     */
+    Pair<Integer, Integer> getPlayerXYOffset();
 }
